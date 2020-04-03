29f7a868f44183a183c789c47f5f7b5633222558
James Waples <james@wapl.es>
2020-04-03 16:38:37 +0100

    Basic readmes

--- event-sauce/src/lib.rs
+++ event-sauce/src/lib.rs
@@ -1,4 +1,10 @@
-//! Event store core crate
+//! # `event-sauce`
+//!
+//! [![Build Status](https://circleci.com/gh/jamwaffles/event-sauce/tree/master.svg?style=shield)](https://circleci.com/gh/jamwaffles/event-sauce/tree/master)
+//! [![Crates.io](https://img.shields.io/crates/v/event-sauce.svg)](https://crates.io/crates/event-sauce)
+//! [![Docs.rs](https://docs.rs/event-sauce/badge.svg)](https://docs.rs/event-sauce)
+//!
+//! Core crate following the event sourcing paradigm.
 
 #![deny(missing_docs)]
 #![deny(intra_doc_link_resolution_failure)]
--- storage-sqlx/src/lib.rs
+++ storage-sqlx/src/lib.rs
@@ -1,4 +1,13 @@
-//! [sqlx](https://docs.rs/sqlx) storage provider for [event-sauce](https://docs.rs/event-sauce)
+//! # Event sauce SQLX storage backend
+//!
+//! [![Build Status](https://circleci.com/gh/jamwaffles/event-sauce/tree/master.svg?style=shield)](https://circleci.com/gh/jamwaffles/event-sauce/tree/master)
+//! [![Docs.rs](https://docs.rs/event-sauce-storage-sqlx/badge.svg)](https://docs.rs/event-sauce-storage-sqlx)
+//!
+//! [sqlx](https://crates.io/crates/sqlx) storage adapter for event-sauce-storage-sqlx.
+//!
+//! ## Features
+//!
+//! - `with-postgres` (enabled by default) - Enable support for Postgres databases by exposing the `SqlxPgStore` storage adapter.
 
 #![deny(missing_docs)]
 #![deny(intra_doc_link_resolution_failure)]
